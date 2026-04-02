5d81facfa005c32e1a0e5bc7e27470af64917456
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-04-02 22:05:31 +0530

    fuel check.c

---
+++ fuel check.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a*b>=100){
+        printf("Yes\n");
+    }
+    else{
+        printf("No\n");
+    }
+    return 0;
+	// your code goes here
+
+}
