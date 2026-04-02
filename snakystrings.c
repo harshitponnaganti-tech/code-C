f6c1bfc6afc3ba4daf91821a5852d2b546ec6115
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-04-02 22:51:37 +0530

    snakystrings.c

---
+++ snakystrings.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() { 
+    char A[4];
+    scanf("%s",&A);
+    if(A[0]=='s'||A[3]=='s'){
+        printf("yes\n");
+    }
+    else{
+        printf("no\n");
+    }
+    return 0;
+
+	// your code goes here
+
+}
+
