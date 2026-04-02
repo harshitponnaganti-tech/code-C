98fc9929af8c974df474682c69e1391a15c78e64
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-04-02 22:32:17 +0530

    cehckstring.c

---
+++ cehckstring.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char s[10];
+    scanf("%s",s);
+    if(s[2]=='n'&&s[3]=='t'&&s[4]=='a'){
+        printf("yes\n");
+    }
+    else{
+        printf("no\n");
+    }
+    return 0;
+	// your code goes here
+
+}
+
