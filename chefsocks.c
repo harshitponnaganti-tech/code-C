e2f6bfd4e19de94894ce38e2bb6598e82d38e9cf
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-04-03 15:04:09 +0530

    chefsocks.c

---
+++ chefsocks.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a<=b+c){
+        printf("yes\n");
+    }
+    else{
+        printf("no\n");
+    }
+    return 0;
+	// your code goes here
+
+}
+
