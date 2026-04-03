602463cc8002cd23f8f0ed9253aa8c0659307852
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-04-03 13:50:35 +0530

    woldcupfinals.c

---
+++ woldcupfinals.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a>=12){
+        printf("yes\n");
+    }
+    else{
+        printf("no\n");
+    }
+    return 0;
+	// your code goes here
+
+}
+
