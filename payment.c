5ae040760d17908c5648df6d34260040303846b4
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-04-03 16:19:49 +0530

    payment.c

---
+++ payment.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a,res;
+    scanf("%d",&a);
+    res=100+(4*a);
+    if(res<300){
+        printf("%d",res);
+    }
+    else{
+        printf("300");
+    }
+    return 0;
+	// your code goes here
+
+}
+
