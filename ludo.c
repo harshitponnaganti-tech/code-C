17b14ab3d8d1c0dd1a5bae589d98cffc267c4dcb
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-03 22:38:36 +0530

    Add ludo.c to check for input equal to 6

---
+++ ludo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int T, X;
+    scanf("%d", &T);
+
+    while (T--) {
+        scanf("%d", &X);
+
+        if (X == 6)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+
+    return 0;
+}
