15e5c78794e7c2749ea0e616122b3103eb898fd4
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-04 22:42:04 +0530

    Add program to calculate Rs. 500 notes
    Implement a program to calculate Rs. 500 notes from an integer input.

---
+++ code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    // Read the single integer N
+    scanf("%d", &N);
+    
+    // Print the equivalent number of Rs. 500 notes
+    printf("%d\n", N * 4);
+    
+    return 0;
+}
