0b4851c29cab4489fa542d25d5a9352de1d22a49
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-04 22:37:41 +0530

    Add audiorange.c to check audio range validity

---
+++ audiorange.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int T, X;
+    scanf("%d", &T);
+
+    while (T--) {
+        scanf("%d", &X);
+
+        if (X >= 67 && X <= 45000)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+
+    return 0;
+}
