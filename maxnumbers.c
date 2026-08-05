f334101c0f82e0cb350b2a7e1f5020a9b31e6f96
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-05 22:53:42 +0530

    Create maxnumbers.c

---
+++ maxnumbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    while (N--) {
+        int a, b, c;
+        scanf("%d %d %d", &a, &b, &c);
+
+        if ((a > b && a < c) || (a < b && a > c))
+            printf("%d\n", a);
+        else if ((b > a && b < c) || (b < a && b > c))
+            printf("%d\n", b);
+        else
+            printf("%d\n", c);
+    }
+
+    return 0;
+}
