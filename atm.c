41c5590eb11671b8fea1dc61b8dd06830830c506
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-09 21:19:30 +0530

    Implement ATM withdrawal functionality

---
+++ atm.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int X;
+    float Y;
+
+    scanf("%d %f", &X, &Y);
+
+    if (X % 5 == 0 && Y >= X + 0.50)
+        Y = Y - X - 0.50;
+
+    printf("%.2f\n", Y);
+
+    return 0;
+}
