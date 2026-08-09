34e77a5724182f49ebfc0f1d0a2ae72cf3d8f269
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-09 18:52:49 +0530

    Add main function to compare two integers

---
+++ alicemarks.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int X, Y;
+    scanf("%d %d", &X, &Y);
+
+    if (X >= 2 * Y)
+        printf("Yes\n");
+    else
+        printf("No\n");
+
+    return 0;
+}
