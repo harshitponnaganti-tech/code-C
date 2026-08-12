cdd7501b3857c6b574844024f564d8287809de52
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-12 22:20:14 +0530

    Implement game logic for Monopoly rules

---
+++ monopoly.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    while (T--) {
+        int R1, R2, R3;
+        scanf("%d %d %d", &R1, &R2, &R3);
+
+        if (R1 > R2 + R3 || R2 > R1 + R3 || R3 > R1 + R2)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+
+    return 0;
+}
