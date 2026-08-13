ee19ff2a2553c58cfd781150cc97c837ff8634b3
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-13 16:20:53 +0530

    Add countnotes.c to calculate and print values

---
+++ countnotes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    while (T--) {
+        int N;
+        scanf("%d", &N);
+
+        printf("%d\n", N * 10);
+    }
+
+    return 0;
+}
