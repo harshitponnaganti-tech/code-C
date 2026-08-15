a64acdaac5747550b96d51b166ce922c6981e3b1
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-15 11:32:00 +0530

    Add testscore.c to evaluate test scores

---
+++ testscore.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    while (T--) {
+        int N, X, Y;
+        scanf("%d %d %d", &N, &X, &Y);
+
+        if (Y % X == 0 && Y <= N * X)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+
+    return 0;
+}
