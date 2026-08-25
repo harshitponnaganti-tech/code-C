6154822098808d70d89c03883a3d90b8106e7370
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-25 23:41:42 +0530

    Add sumordiff.c for sum and difference calculation

---
+++ sumordiff.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int N1, N2;
+    scanf("%d %d", &N1, &N2);
+
+    if (N1 > N2)
+        printf("%d\n", N1 - N2);
+    else
+        printf("%d\n", N1 + N2);
+
+    return 0;
+}
