ac2797d0bd275ae1be2bed383f93e425cfbe85db
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-09-27 00:24:38 +0530

    Create summertime.c

---
+++ summertime.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    scanf("%d",&i);
+    if(i>35){
+        printf("yes");
+    }
+    else{
+        printf("no");
+    }
+    return 0;
+	// your code goes here
+
+}
+
